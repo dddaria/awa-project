3b7ffe46645220616a3183e0016fd80103307274
cajlie <liedbergcajsa@gmail.com>
2021-04-26 16:43:19 +0200

    qui

--- header.php
+++ header.php
@@ -25,55 +25,63 @@
 	<header>
 			<div id="topnavDIV">
 				<ol class="top-nav">
-					<li>
-					<div class="logodiv">
-					<a href="index.php"><img src="img/logo.png" class="logoheader" class="
-                    <?php 
-					// current or '' because it is the Index page
-					echo($currentPage == 'index.php' || $currentPage == '') ? 'activelink' :''?>"> </a>
+					<div class="header_right">
+						<li>
+							<div class="logodiv">
+								<a href="index.php"><img src="img/logo.png" class="logoheader" class="
+								<?php 
+								// current or '' because it is the Index page
+								echo($currentPage == 'index.php' || $currentPage == '') ? 'activelink' :''?>"> </a>
+							</div>
+						</li>
 					</div>
-					</li>
-                    <li>
-						<h1><a href="popular-destinations.php" class="<?php
-						echo ($currentPage == 'popular-destinations.php')?'activelink' :''
-						?>">Popular </a> </h1>
-					</li>
-					<li>
-						<h1><a href="all-destinations.php"  class="<?php echo($currentPage == 'all-destinations.php') ? 'activelink' :''?>">All Destinations</a></h1>
-                    </li>
-                    <li>
-						<h1><a href="random-destination.php" class="<?php
-						echo ($currentPage == 'random-destination.php' )?'activelink' :''
-						?>">Randomize</a></h1>
-					</li>
+				
+					<div class="header_middle">
+						<li>
+							<h1><a href="popular-destinations.php" class="<?php
+							echo ($currentPage == 'popular-destinations.php')?'activelink' :''
+							?>">Popular </a> </h1>
+						</li>
+						<li>
+							<h1><a href="all-destinations.php"  class="<?php echo($currentPage == 'all-destinations.php') ? 'activelink' :''?>">All Destinations</a></h1>
+						</li>
+						<li>
+							<h1><a href="random-destination.php" class="<?php
+							echo ($currentPage == 'random-destination.php' )?'activelink' :''
+							?>">Randomize</a></h1>
+						</li>
 
-					<li>
-						<h1><a href="about-us.php" class="<?php
-						echo ($currentPage == 'about-us.php' )?'activelink' :''
-						?>">About Us</a></h1>
-					</li>
-			
-					<li>
-						<div class="search-container">
-						<form action="">
-						<input type="text" placeholder="Search.." name="search">
-						</form>
+						<li>
+							<h1><a href="about-us.php" class="<?php
+							echo ($currentPage == 'about-us.php' )?'activelink' :''
+							?>">About Us</a></h1>
+						</li>
 					</div>
-					</li>
-					
-					<li>
-                        <div class="fav-div">
-					<a href="favorite-destinations.php"><img src="img/favorite-icon.png" class="fav-icon" class="
-                    <?php
-						echo ($currentPage == 'favorite-destinations.php' )?'activelink' :''
-						?>"> </a>
-                        </div>
-					<li>
-						<h1><a href="login.php" class="<?php
-						echo ($currentPage == 'login.php' )?'activelink' :''
-						?>">Log in</a></h1>
-					</li>
-					</li>
+				
+					<div class="header_left">
+						<li>
+							<div class="search-container">
+								<form action="">
+									<input type="text" placeholder="Search.." name="search">
+								</form>
+						</div>
+						</li>
+						
+						<li>
+							<div class="fav-div">
+								<a href="favorite-destinations.php"><img src="img/favorite-icon.png" class="fav-icon" class="
+								<?php
+								echo ($currentPage == 'favorite-destinations.php' )?'activelink' :''
+								?>"> </a>
+								</div>
+							</li>
+						<li>
+							<h1><a href="login.php" class="<?php
+							echo ($currentPage == 'login.php' )?'activelink' :''
+							?>">Log in</a></h1>
+						</li>
+						
+					</div>
 				</ol>
 			</div> <!-- end of header div -->
 		</header>	
