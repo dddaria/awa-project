70c9c9dadafe6b0c6864ada73d6518c74bdbc2be
cajlie <liedbergcajsa@gmail.com>
2021-04-29 16:42:22 +0200

    startat med all destinations

--- all-destinations.php
+++ all-destinations.php
@@ -1,5 +1,29 @@
 <?php 
 include('header.php');?>
+<body>
+	<main>
+		<div class="destination-main">
+			<ol class="all-destinations-ol">
 
+<!---Destination list--->
+				<li class="destination-li">
+					<a href="../David/ImDrunk.html">
+					<div class="destination-img-div">
+						<img class="destination-img" src="img/bigben.jpg">
+						<!-- <img class="second-img" src="Photos/hover/capshover.png"> -->
+					</div>
+						<div class="patch-info">
+							<h3 class="patch-name-h3">Big Ben</h3>
+							<p class="info-text-p">London, United Kingdom</p>
+							<button class="visit-button"><a href="#">Visit</a></button>
+						</div>
+					</a>
+				</li>
+
+
+			</ol>
+		</div>
+	</main>
+</body>
 <?php 
 include('footer.php');?>
