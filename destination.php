c1c39f5fbcff9014d13cbe2605464d12d0ba158a
cajlie <liedbergcajsa@gmail.com>
2021-05-01 23:02:53 +0200

    destination

--- destination.php
+++ destination.php
@@ -1,5 +1,25 @@
 <?php 
 include('header.php');?>
+<body>
+    
+<div class="destination-div">
+        <!-- <?php
+        echo "<div class='destination-img'> $destinationImg </div>";
+        echo "<div class='destination-text-div'> 
+            <h1> $destinationName </h1>
+            <p> $destinationText</p> </div>";
+        ?> -->
+       
+        <div class="destination-img"> 
+            <p> destinationImg </p>
+        </div>
 
+        <div class="destination-text-div"> 
+                <h1> destinationName </h1>
+                <p> destinationText</p>
+        </div>
+   
+</div>
+</body>
 <?php 
 include('footer.php');?>
