f33ca1c1f8f7c4ca7d1a063d366940af986d41fd
sarammarias <klsa18ur@student.ju.se>
2021-05-06 13:05:38 +0200

    all dest

--- all-destinations.php
+++ all-destinations.php
@@ -1,3 +1,98 @@
 <?php include('header.php');?>
 
+<!DOCTYPE html>
+
+<html>
+	
+
+  <main class="destGrid">
+    <ul>
+      	<li>
+        	<div class="text-on-img">
+         	<a href="/awa-project/all-destinations.php">
+         	<img src="img/eiffeltower.jpeg" class="img-fix" /></a>
+        	</div>
+      	</li>
+      	<li>
+        	<div class="text-on-img">
+          	<a href="/awa-project/all-destinations.php">
+            <img src="img/angkorWat.jpeg" class="img-fix" /></a>
+        </div>
+      </li>
+      <li>
+        <div class="text-on-img">
+          <a href="/awa-project/all-destinations.php">
+            <img src="img/forbiddencity.jpg" class="img-fix" />
+          </a>
+        </div>
+      </li>
+      <li>
+        <div class="text-on-img">
+          <a href="/awa-project/all-destinations.php">
+            <img src="./img/fiji.jpg" class="img-fix" />
+          </a>
+        </div>
+      </li>
+      <li>
+        <div class="text-on-img">
+          <a href="/awa-project/all-destinations.php">
+            <img src="./img/gibbon-experience.jpg" class="img-fix" />
+          </a>
+        </div>
+      </li>
+      <li>
+        <div class="text-on-img">
+          <a href="/awa-project/all-destinations.php">
+            <img src="./img/seven-noses-soho.jpg" class="img-fix" />
+          </a>
+        </div>
+      </li>
+      <li>
+        <div class="text-on-img">
+          <a href="/awa-project/all-destinations.php">
+            <img src="./img/crooked-forest-poland.jpg" class="img-fix" />
+          </a>
+        </div>
+      </li>
+      <li>
+        <div class="text-on-img">
+          <a href="/awa-project/all-destinations.php">
+            <img src="./img/cameron-highlands.jpg" class="img-fix" />
+          </a>
+        </div>
+      </li>
+      <li>
+        <div class="text-on-img">
+          <a href="/awa-project/all-destinations.php">
+            <img src="./img/cats-island.jpg" class="img-fix" />
+          </a>
+        </div>
+      </li>
+      <li>
+        <div class="text-on-img">
+          <a href="/awa-project/all-destinations.php">
+            <img src="./img/lake-hillier.jpg" class="img-fix" />
+          </a>
+        </div>
+      </li>
+      <li>
+        <div class="text-on-img">
+          <a href="/awa-project/all-destinations.php">
+            <img src="./img/karpathos-olympos.jpg" class="img-fix" />
+          </a>
+        </div>
+      </li>
+      <li>
+        <div class="text-on-img">
+          <a href="/awa-project/all-destinations.php">
+            <img src="./img/stintino.jpg" class="img-fix" />
+          </a>
+        </div>
+      </li>
+    </ul>
+  </main>
+	
+
+</html>
+
 <?php include('footer.php');?>
