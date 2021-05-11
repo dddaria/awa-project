f00a0defc57b6de9fd0afdf2307e27bdec77c6ed
dddaria <daria.lewandowska24@gmail.com>
2021-05-11 22:45:38 +0200

    dropdown with cities added

--- add-new-destination.php
+++ add-new-destination.php
@@ -4,19 +4,44 @@
     <h1>WELCOME ADMIN!</h1>
     <p>Add new destination:</p><br>
     <form id="desinationUpload" name="destinationUpload" method="post" enctype="multipart/form-data">
-        Name: <input type="text" name="destName" id="destName"> <br>
-        Description: <textarea type="text" name="destDes" id="destDes"></textarea> <br>
-        City: <select name="destCity" id="destCity">
-                <option value="" selected="selected"></option>
-            </select> <br>
-        Select image to upload: <br>
+        <label for="destName">Name:</label>
+        <input type="text" name="destName" id="destName"> <br>
+        
+        <label for="destDes">Description:</label>
+        <textarea type="text" name="destDes" id="destDes"></textarea> <br>
+        
+        <label for="destCity">City:</label>
+        <select name="destCity" id="destCity">
+            <?php
+                $stmt = $dbConn->prepare("SELECT CityID, Name FROM City");
+                $stmt->bind_result($CityID, $Name);
+                $stmt->execute();
+
+                while ($stmt->fetch()) {
+                    echo "<option value=".$CityID.">".$Name."</option>";
+                };
+            ?>
+        </select><br>
+        
+        <label for="destImg">Select image to upload:</label> <br>
         <input type="file" name="destImg" id="destImg"> <br>
+        
         <input type="submit" value="Upload destination" name="submitDest">
     </form>
     <?php 
-        if (isset($_POST['submitImage'])) {
-            fileUpload($_FILES['myFile']);
+        if (isset($_POST['submitDest'])) {
+            destUpload($_POST['destName'], $_POST['destDes'], $_POST['destCity'], $_FILES['destImg']);
         }
+        
+        /* test to see if connection to DB works
+        $stmt = $dbConn->prepare("SELECT Name FROM Destination");
+        $stmt->bind_result($Name);
+        $stmt->execute();
+
+        while ($stmt->fetch()) {
+            echo "<p>".$Name."</p>";
+        };
+        */
     ?>
 </main>
 
