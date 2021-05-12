acea8776f36c70f043356e1a89b3c81786e71aef
cajlie <liedbergcajsa@gmail.com>
2021-05-12 19:44:28 +0200

    profile page

--- account.php
+++ account.php
@@ -1,3 +1,34 @@
 <?php include('header.php');?>
+<!-- <?php
+echo '';
+?> -->
+<div class="profile-page">
+    <div class="profile-info-div"> 
+        <h2 class="profile-header"> Welcome Cajsa</h2>
+                <table class="profile-table"> 
+                        <tr>
+                            <th>Username</th> <td>lica18</td>
+                        </tr>
+                        <tr>
+                            <th>Password</th> <td>hemligt</td>
+                        </tr>
+                        <tr>
+                            <th>Email</th> <td>lång</td>
+                        </tr>
+                        <tr>
+                            <th>Phone</th> <td>siffror</td>
+                        </tr>
+                        <tr>
+                            <th>Nationality</th> <td>Östgötsk</td>
+                        </tr>
+                </table>
+    </div>
 
+    <div class="profile-right-div">
+        <div class="profile-img-div">
+            <img class="profile-img" src="img/eiffel.jpeg">
+        </div> 
+        <p class="change-profile-img" ><a>Change profile picture</a></p>
+    </div>
+</div> 
 <?php include('footer.php');?>
