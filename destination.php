e58e1d279304edfb9948702b4ec14ed21f259650
cajlie <liedbergcajsa@gmail.com>
2021-05-12 18:22:43 +0200

    css

--- destination.php
+++ destination.php
@@ -9,13 +9,20 @@ include('header.php');  ?>
             <p> $destinationText</p> </div>";
         ?> -->
        
-        <div class="destination-img"> 
-            <p> destinationImg </p>
-        </div>
+      
 
+        <div class="destination-fact-div"> 
+                <h1> destinationName FRANCE, EIFFELTOWER </h1>
+                <p> Country: France
+                    <br>
+                    Money:none
+                </p>
+        </div>
+        <div class="destination-img"> 
+            <img src="img/eiffel.jpeg">
+        </div>
         <div class="destination-text-div"> 
-                <h1> destinationName </h1>
-                <p> destinationText</p>
+                <p> destinationText France, in Western Europe, encompasses medieval cities, alpine villages and Mediterranean beaches. Paris, its capital, is famed for its fashion houses, classical art museums including the Louvre and monuments like the Eiffel Tower. The country is also renowned for its wines and sophisticated cuisine. Lascaux’s ancient cave drawings, Lyon’s Roman theater and the vast Palace of Versailles attest to its rich history.</p>
         </div>
    
 </div>
@@ -23,7 +30,3 @@ include('header.php');  ?>
 
 <?php 
 include('footer.php');?>
-=======
-<?php include('header.php');?>
-
-<?php include('footer.php');?>
