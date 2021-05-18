a69fd763ecb6c0d41d54bdd58b008025613ca0de
sarammarias <klsa18ur@student.ju.se>
2021-05-18 17:05:21 +0200

    Login funkar nu, session tillagda och log out button

--- all-destinations.php
+++ all-destinations.php
@@ -10,82 +10,82 @@
       	<li>
         	<div class="text-on-img">
          	<a href="/awa-project/all-destinations.php">
-         	<img src="img/eiffeltower.jpeg" class="img-fix" /></a>
+         	<img src="" class="img-fix" /></a>
         	</div>
       	</li>
       	<li>
         	<div class="text-on-img">
           	<a href="/awa-project/all-destinations.php">
-            <img src="img/angkorWat.jpeg" class="img-fix" /></a>
+            <img src="" class="img-fix" /></a>
         </div>
       </li>
       <li>
         <div class="text-on-img">
           <a href="/awa-project/all-destinations.php">
-            <img src="img/forbiddencity.jpg" class="img-fix" />
+            <img src="" class="img-fix" />
           </a>
         </div>
       </li>
       <li>
         <div class="text-on-img">
           <a href="/awa-project/all-destinations.php">
-            <img src="./img/fiji.jpg" class="img-fix" />
+            <img src="" class="img-fix" />
           </a>
         </div>
       </li>
       <li>
         <div class="text-on-img">
           <a href="/awa-project/all-destinations.php">
-            <img src="./img/gibbon-experience.jpg" class="img-fix" />
+            <img src="" class="img-fix" />
           </a>
         </div>
       </li>
       <li>
         <div class="text-on-img">
           <a href="/awa-project/all-destinations.php">
-            <img src="./img/seven-noses-soho.jpg" class="img-fix" />
+            <img src="" class="img-fix" />
           </a>
         </div>
       </li>
       <li>
         <div class="text-on-img">
           <a href="/awa-project/all-destinations.php">
-            <img src="./img/crooked-forest-poland.jpg" class="img-fix" />
+            <img src="" class="img-fix" />
           </a>
         </div>
       </li>
       <li>
         <div class="text-on-img">
           <a href="/awa-project/all-destinations.php">
-            <img src="./img/cameron-highlands.jpg" class="img-fix" />
+            <img src="" class="img-fix" />
           </a>
         </div>
       </li>
       <li>
         <div class="text-on-img">
           <a href="/awa-project/all-destinations.php">
-            <img src="./img/cats-island.jpg" class="img-fix" />
+            <img src="" class="img-fix" />
           </a>
         </div>
       </li>
       <li>
         <div class="text-on-img">
           <a href="/awa-project/all-destinations.php">
-            <img src="./img/lake-hillier.jpg" class="img-fix" />
+            <img src="" class="img-fix" />
           </a>
         </div>
       </li>
       <li>
         <div class="text-on-img">
           <a href="/awa-project/all-destinations.php">
-            <img src="./img/karpathos-olympos.jpg" class="img-fix" />
+            <img src="" class="img-fix" />
           </a>
         </div>
       </li>
       <li>
         <div class="text-on-img">
           <a href="/awa-project/all-destinations.php">
-            <img src="./img/stintino.jpg" class="img-fix" />
+            <img src="" class="img-fix" />
           </a>
         </div>
       </li>
--- login.php
+++ login.php
@@ -1,93 +1,66 @@
 <?php include('header.php');?>
 
-<!DOCTYPE html>
 
-<html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="style.css">
-	</head>
-
-	<body> 
-
+<?php
+	if($_SESSION["loggedin"] === "true"){
+	header("location:/awa-project/account.php");
+	}
+?>
 		<div class=loginBox>
 			
 			<h1>SIGN IN</h1>
 
 				<form class="LoginForm" action="" method="POST"> <!-- Form för login. POST skickar info, ej via url-->
-					<input type="text" name="uname" placeholder="Username"> <br>
-					<input type="password" name="pass" placeholder="Password"> <br>
+					<input type="text" placeholder="Username" name="uname" required> <br>
+					<input type="password" placeholder="Password" name="pass" required> <br>
 					<a href="sign-up.php" class="SignUpHere"> Don't have an account yet? Sign up here!</a>
 					<input type="submit" name="Login">
 				</form>
 
-				
-
 		</div>
 
+
 		<?php
-
-
-			if(empty($_POST['uname']) && isset($_POST['pass'])){
-				echo"Please enter username";
-			}
-			else if(empty($_POST['pass']) && isset($_POST['uname'])){
-				echo"Please enter password";
-			}
-			else if(isset($_POST['uname']) && isset($_POST['pass'])){ //kollar så att både uname och pw är set i form
-				$uname = $_POST['uname']; //om användaren fyllt i båda fälten så lägger vi in värdena i variabeln
-				$pass = $_POST['pass'];
-				//$query = "SELECT * from person WHERE Username = '$uname' AND Password = '$pass'"; //osäkert sätt
+				if(isset($_POST['uname']) && isset($_POST['pass'])){ //kollar så att både uname och pw är set i form
+					$uname = $_POST['uname']; //om användaren fyllt i båda fälten så lägger vi in värdena i variabeln
+					$pass = $_POST['pass'];
 
 				$uname = htmlspecialchars($uname, ENT_QUOTES,'UTF-8'); //skyddar från att attackera hemsidan. hindrar script. 
 				$pass = htmlspecialchars($pass, ENT_QUOTES,'UTF-8');
 
 				
-				$query = "SELECT * from person WHERE Username = ? AND Password = ?"; //hämtar värden från db ? så att man inte kan hämta hela listan
-
-				//echo "the input was: $uname";
-
-				
+				$query = "SELECT * from User WHERE Username = ? AND Password = ?"; //hämtar värden från db ? så att man inte kan hämta hela listan
+			
 
 				$stmt = $dbConn->prepare($query);
 				$stmt->bind_param('ss',$uname, $pass);//ss- två strings från input skyddar oss från SQL injection
-				$stmt->bind_result($PersonSecNr,$FirstName,$LastName,$Usr,$Pw,$UserType); //lägger ihop de värden som hämtats
+				$stmt->bind_result($UserID,$Username,$Password,$Usertype,$Email,$Fullname); //lägger ihop de värden som hämtats
 				$stmt->execute();
 
-
-					echo '<br><table style="margin-left:auto; margin-right:auto;" bgcolor="dddddd" cellpadding="15">';
-					echo '<tr><b><th>Username</th> <th>Password</th></b></tr>';
-						while($stmt->fetch()){
-							echo "<tr>";
-							echo "<td> $Usr </td><td> $Pw </td>";
-							echo "</tr>";
-						}
-					echo "</table>";
-
+				while($stmt->fetch());
 				$stmt->close();
 
-				if($uname === $Usr && $pass===$Pw && $UserType==="Admin"){ //kollar så att input är lika med databasen. Admin?
-					session_start();
 
-					$_SESSION["loggedin"]=true;
-					$_SESSION["UserName"]=$Usr;
-					$_SESSION["UserType"]=$UserType;
+				if($uname !== $Username && $pass !== $Password){ 
+				 	echo "Sorry wrong credentials. Please try again.";
+				
+				}else if($uname === $Username && $pass === $Password && $Usertype === "Admin"){ //kollar så att input är lika med databasen. Admin?
+					
+					$_SESSION["loggedin"] = "true";
+					$_SESSION["UserName"] = $Username;
+					$_SESSION["UserType"] = $Usertype;
 
-
-					header("location:/awa-project/admin.php");
-				 }else if($uname === $Usr && $pass===$Pw && $UserType==="Normal"){ 
-				 	session_start();
-
-					$_SESSION["loggedin"]=true;
-					$_SESSION["UserName"]=$Usr;
-					$_SESSION["UserType"]=$UserType;
 
 					header("location:/awa-project/account.php");
-				 }
-				 else{
-				 	echo"You entered the wrong login. Please try again!";
-				 }
 
-			
+				}else if($uname === $Username && $pass === $Password && $Usertype === "Normal"){ 
+				 	
+					$_SESSION["loggedin"] = "true";
+					$_SESSION["UserName"] = $Username;
+					$_SESSION["UserType"] = $UserType;
+
+					header("location:/awa-project/account.php");
+				}
 
 			}
 
@@ -96,8 +69,8 @@
 
 
 
-	</body>
 
-</html>
+
+
 
 <?php include('footer.php');?>
