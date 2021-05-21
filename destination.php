6b477057bb7f9138f91eb12636715a4967fe6584
cajlie <liedbergcajsa@gmail.com>
2021-05-21 13:49:10 +0200

    destination show

--- destination.php
+++ destination.php
@@ -13,49 +13,35 @@ include('header.php');  ?>
         $stmt->bind_result($destName, $destDes, $destPic, $cityName, $countryName, $contentName);
         $stmt->execute();
         $stmt->fetch();
-
-        echo $destName, $destDes, $destPic, $cityName, $countryName, $contentName;
 ?>
 <div class="destination-page">
     <div class="destination-top-div">
         <div class="destination-info-div"> 
-            <!-- <?php
-                echo '        <h2 class="destination-header"> Welcome to $place !</h2>
+            <?php
+                echo ' <h2 class="destination-header"> Welcome to '.$destName.' !</h2>
                     <table class="destination-table"> 
                         <tr>
-                            <th>Country:</th> <td>$country</td>
+                            <th>Country:</th> <td>'.$countryName.'</td>
                         </tr>
                         <tr>
-                            <th>Place:</th> <td>$place</td>
+                            <th>City:</th> <td>'.$cityName.'</td>
                         </tr>
-                        <tr>
-                            <th>Money</th> <td>$money</td>
-                        </tr>
-                    </table>
-        </div>';
-            ?> -->
-
-            <h2 class="destination-header"> Welcome to France!</h2>
-                    <table class="destination-table"> 
-                            <tr>
-                                <th>Country:</th> <td>France</td>
-                            </tr>
-                            <tr>
-                                <th>Place:</th> <td>towah</td>
-                            </tr>
-                            <tr>
-                                <th>Money</th> <td>none</td>
-                            </tr>
-                    </table>
-        </div>
-                <div class="destination-img-div">
-                    <img class="destination-img" src="img/eiffel.jpeg">
-                </div> 
-    </div>
+                    </table> ';
+            ?> 
+         </div>
 
             <div class="destination-text-div"> 
-                    <p> destinationText France, in Western Europe, encompasses medieval cities, alpine villages and Mediterranean beaches. Paris, its capital, is famed for its fashion houses, classical art museums including the Louvre and monuments like the Eiffel Tower. The country is also renowned for its wines and sophisticated cuisine. Lascaux’s ancient cave drawings, Lyon’s Roman theater and the vast Palace of Versailles attest to its rich history.</p>
-            </div>
+                   <?php 
+                   echo' <p> '.$destDes.'</p>';
+           
+                ?>
+           </div>
+           <div class="destination-image-div"> 
+                   <?php 
+                   echo' <div> '.$destPic.'</div>';
+           
+                ?>
+           </div>
             
        
         <?php
