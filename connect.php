051a788c6b5c204a77b27e6fc05ade7a5a86f830
dddaria <daria.lewandowska24@gmail.com>
2021-05-27 13:47:45 +0200

    session hijacking protection added

--- connect.php
+++ connect.php
@@ -13,4 +13,15 @@ $dbConn = mysqli_connect($host, $user, $password, $database);
 if($dbConn->connect_errno) {
     echo "Failed to connect to database:" . $dbConn->connect_error;
 };
-//echo "Connected successfully"; 
+
+//Session hijacking protection
+if(isset($_SESSION["UserIP"])) {
+    //Checking if session isn't hijacked
+    if($_SESSION["UserIP"] !== $_SERVER['REMOTE_ADDR']) {
+        //If session is hijacked, destroy it
+        session_unset();
+        session_destroy();
+        //Start new session
+        session_start();
+    };
+};
