cc4c5c54a620eedf4a83b6b73e661de2f08b9b42
sarammarias <klsa18ur@student.ju.se>
2021-06-01 09:32:11 +0200

    about us

--- about-us.php
+++ about-us.php
@@ -2,6 +2,15 @@
 
 <main class="aboutUs">
 
+<div class="UsContainer">
+	<h1>Explore & More Team</h1>
+
+	<p class="UsText">We all have a passion for travelling and exploring the world, something that we want more people to do! Learning about new cultures and getting out of your comfort-zone is something we believe is important for the modern world. Therefore we have created this website with the purpose of bringing life into travelling and inspiring others to exploring new places; however, it's not only that, it's also a platform for discussions and tips! Each destination-page provides you with the essential information before travelling. Enjoy! </p>
+
+	<img src="img/us.jpg" class="usImg">
+</div>
+
+
 </main>
 
 
--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
 	<div class="footertext">
-		<h4> Cajsa Daria Sara | All rights reserved </h4>
+		<h4> Daria Sara Cajsa | All rights reserved </h4>
 		<img src="img/explore.png" />
 		<h4> &copy; 2021 | Explore & More </h4>
 	</div>
